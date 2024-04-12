b4725caf9c5c5e606473215e486765450f4852d6
Jacob <87302129+py-er@users.noreply.github.com>
2024-04-12 18:27:00 +0200

    fix

--- app/luftpistol/views.py
+++ app/luftpistol/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
 
-from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder
-from picamera2.outputs import FileOutput
-
-picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
-output = "stream.mjpg"
-picam2.start_recording(JpegEncoder(), FileOutput(output))
-
 # Create your views here.
 
 def stream_view(request):
