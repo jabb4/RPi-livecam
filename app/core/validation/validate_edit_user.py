0935ebdf119a76fc396bab96329829f5da35d5e5
Jacob <87302129+py-er@users.noreply.github.com>
2024-04-13 10:33:19 +0200

    it was not good

--- app/core/validation/validate_edit_user.py
+++
@@ -1,8 +0,0 @@
-from .validate_password import testCurrentPassword
-from .validate_username import testUsernameExistence
-
-def testEditUser(request, email, password):
-    if testCurrentPassword(request=request, password=password):
-        return testCurrentPassword(request=request, password=password)
-    elif testUsernameExistence(request=request, email=email):
-        return testUsernameExistence(request=request, email=email)
--- app/luftpistol/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LuftpistolConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'luftpistol'
