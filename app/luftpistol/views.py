8000c8fa9e5de1c45beaea25e2dea2e254d25eba
Jacob <87302129+py-er@users.noreply.github.com>
2024-04-22 20:14:06 +0200

    fixes

--- app/luftpistol/views.py
+++ app/luftpistol/views.py
@@ -7,13 +7,13 @@ def start_stream():
     process = subprocess.run(["python","cameraStream.py"])
 
 def stop_stream():
-    process = subprocess.run(["./kill_cam.sh"])
+    process = subprocess.run(["./app/luftpistol/kill_cam.sh"])
 
 def take_pic():
     pass
 
 def stream_view(request):
-    stop_stream()
+    #stop_stream()
     
     context = {
         
