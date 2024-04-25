59a9336dec7e9710e462b8a20b308761f927109f
Jacob <87302129+py-er@users.noreply.github.com>
2024-04-25 22:37:34 +0200

    design

--- app/static/static/js/stream.js
+++ app/static/static/js/stream.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-
+    const loader = document.querySelector(".loader-div");
     loader.classList.add("loader--hidden");
-
     loader.addEventListener("transitionend", () => {
         document.body.removeChild(loader);
     });
+    var main = document.getElementById("main");
+    main.style.display = "flex";
 });
