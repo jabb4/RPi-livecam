1f27ed11a23ed92d978bc73705642f682c763c25
Jacob <87302129+py-er@users.noreply.github.com>
2024-06-13 13:14:29 +0200

    Changed paths to make it more user-friendly

--- app/luftpistol/urls.py
+++ app/luftpistol/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("live/", views.stream_view, name="stream"),
-    path("serier/", views.saved_series_view, name="saved"),
+    path("", views.stream_view, name="stream"),
+    path("saved/", views.saved_series_view, name="saved"),
 ]
